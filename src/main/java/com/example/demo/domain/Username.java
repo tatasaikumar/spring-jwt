29f3d44e3af94876151b4d3bd313def167957382
sai kumar t <tatasaikumar1@gmail.com>
2023-08-21 11:41:25 -0400

    jwt example

---
+++ src/main/java/com/example/demo/domain/Username.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Embeddable;
+
+@Data
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+public class Username {
+    private String username;
+}
